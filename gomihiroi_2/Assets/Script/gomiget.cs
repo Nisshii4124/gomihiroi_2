167e6bcd55ee56a92059bd1e2e183323964b0d5a
HaruRabbi <nkc.harutaka.chikazawa@gmail.com>
2017-02-20 14:51:20 +0900

    ゴミ袋追加

--- gomihiroi_2/Assets/Script/gomiget.cs
+++ gomihiroi_2/Assets/Script/gomiget.cs
@@ -20,23 +20,23 @@ public class gomiget : MonoBehaviour
 
     public void OnTriggerStay(Collider other)
     {
-        string label = " ";
+       // string label = " ";
         if (TimeandScore.gomi <= TimeandScore.gomimax)
         {
             if (other.gameObject.tag == "gomi")//tag「gomi」のついたオブジェクトにふれたら
             {
-                if (Input.GetKey(KeyCode.B))
+                if (Input.GetKey(KeyCode.UpArrow))
                 {
                     Destroy(other.gameObject);//オブジェクトを消す
                     TimeandScore.gomi += 1;
-                    label = "ゴミを拾うよ～";
+                   // label = "ゴミを拾うよ～";
                 }
-                GUI.TextField(new Rect(10, 10, 100, 100), label);
+                //GUI.TextField(new Rect(10, 10, 100, 100), label);
             }
         }
         if (other.gameObject.tag == "gomibako")//tag「gomibako」のついたオブジェクトにふれたら
         {
-            if (Input.GetKeyDown(KeyCode.B))
+            if (Input.GetKeyDown(KeyCode.DownArrow))
             {
                 sutetagomi = TimeandScore.gomi;
                 TimeandScore.score += sutetagomi * 100;
