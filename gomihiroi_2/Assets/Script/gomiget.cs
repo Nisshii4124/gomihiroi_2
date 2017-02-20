e9e8684ea0860c28bc78f083d9b466629fc01402
nakashima <nkc.kazuki.nakashima@gmail.com>
2017-02-20 13:45:18 +0900

    no message

--- gomihiroi_2/Assets/Script/gomiget.cs
+++ gomihiroi_2/Assets/Script/gomiget.cs
@@ -20,7 +20,7 @@ public class gomiget : MonoBehaviour
 
     public void OnTriggerStay(Collider other)
     {
-        string label = " ";
+       // string label = " ";
         if (TimeandScore.gomi <= TimeandScore.gomimax)
         {
             if (other.gameObject.tag == "gomi")//tag「gomi」のついたオブジェクトにふれたら
@@ -29,9 +29,9 @@ public class gomiget : MonoBehaviour
                 {
                     Destroy(other.gameObject);//オブジェクトを消す
                     TimeandScore.gomi += 1;
-                    label = "ゴミを拾うよ～";
+                  //  label = "ゴミを拾うよ～";
                 }
-                GUI.TextField(new Rect(10, 10, 100, 100), label);
+               // GUI.TextField(new Rect(10, 10, 100, 100), label);
             }
         }
         if (other.gameObject.tag == "gomibako")//tag「gomibako」のついたオブジェクトにふれたら
