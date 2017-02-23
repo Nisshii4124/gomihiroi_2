b60c2caac58722c0cd001d8a5d531cc1fd23c0b1
HaruRabbi <nkc.harutaka.chikazawa@gmail.com>
2017-02-23 15:02:44 +0900

    スライド追加(実装はまだ)

---
+++ gomihiroi_2/Assets/Script/Sutamina.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class Sutamina : MonoBehaviour {
+
+    public float Dash;    //ダッシュの倍率
+    public bool DashFlag; //ダッシュできるかどうか
+
+    public float sutamina;//スタミナ
+    Slider SUTAMINA;
+
+    testmove player;
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        SUTAMINA = GameObject.Find("Slider").GetComponent<Slider>();
+        player = GetComponent<testmove>();
+        
+
+        sutamina = SUTAMINA.maxValue;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        SUTAMINA.value = sutamina;
+
+        if (SUTAMINA.value != 0)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift)&&rb.velocity.magnitude!=0)
+            {
+                sutamina -= Time.deltaTime;
+                
+            }
+        }
+
+	}
+}
